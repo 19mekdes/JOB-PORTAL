3a493a6a9e29497831594bf8c21b04345e4d354e
mekdes wale <mekdesw60@gmail.com>
2026-06-11 09:05:42 +0300

    modified

--- job-portal-frontend/src/components/employer/ApplicationDetails.tsx
+++ job-portal-frontend/src/components/employer/ApplicationDetails.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-hooks/immutability */
 import React, { useState, useEffect } from 'react'
--- job-portal-frontend/src/hooks/useApplications.ts
+++ job-portal-frontend/src/hooks/useApplications.ts
@@ -1,7 +1,6 @@
 /* eslint-disable react-hooks/immutability */
 /* eslint-disable react-hooks/set-state-in-effect */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-
 import { useState, useEffect, useCallback } from 'react'
 import { useSelector } from 'react-redux'
 import { RootState } from '../redux/store'
